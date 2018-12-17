819448ebd9e3348908fd7d022c7347feead0a16a
Tomek Buslowski <tbuslowski@gmail.com>
2018-12-17 18:49:40 +0000

    Updates

---
+++ precache-manifest.d6381dfe315c0f9096f189b52df9ef86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d45432ef7017a195eb1",
+    "url": "/sir-map-app/static/js/runtime~main.3d45432e.js"
+  },
+  {
+    "revision": "6fb2632f5c7f75cf6663",
+    "url": "/sir-map-app/static/js/main.6fb2632f.chunk.js"
+  },
+  {
+    "revision": "c346b1ca2d966c647b67",
+    "url": "/sir-map-app/static/js/1.c346b1ca.chunk.js"
+  },
+  {
+    "revision": "6fb2632f5c7f75cf6663",
+    "url": "/sir-map-app/static/css/main.6d801eea.chunk.css"
+  },
+  {
+    "revision": "3161caaae1081e5d3b19f0144a3d6c59",
+    "url": "/sir-map-app/index.html"
+  }
+];
